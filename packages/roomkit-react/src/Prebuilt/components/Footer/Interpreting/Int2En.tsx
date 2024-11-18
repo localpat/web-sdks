a1d228b770f9797adbbf2e5f8a1ce9eb38f74a0b
Patrick Stainier <patrick.stainier@gmail.com>
2024-11-18 16:03:00 -0400

    feat: removed button states

--- packages/roomkit-react/src/Prebuilt/components/Footer/Interpreting/Int2En.tsx
+++ packages/roomkit-react/src/Prebuilt/components/Footer/Interpreting/Int2En.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { selectLocalPeerID, selectLocalPeerRole, useHMSActions, useHMSStore } from '@100mslive/react-sdk';
+import { selectLocalPeerID, useHMSActions, useHMSStore } from '@100mslive/react-sdk';
 import { Tooltip } from '../../../../Tooltip';
 import IconButton from '../../../IconButton';
 
@@ -9,12 +9,11 @@ const Int2EnButton = () => {
   const changeRole = () => {
     hmsActions.changeRoleOfPeer(localPeerId, 'terp-en', true);
   };
-  const role = useHMSStore(selectLocalPeerRole);
+
   return (
     <Tooltip key="terp-en" title="Interpreting to English">
       <IconButton onClick={changeRole}>
-        {role?.name === 'terp-fr' ? <div className="text-xs text-slate-200 rounded-lg">EN</div> : null}
-        {role?.name === 'terp-en' ? <div className="text-xs text-slate-200 rounded-lg bg-red-600">EN</div> : null}
+        <div className="text-xs text-slate-200 rounded-lg active:bg-red-600">EN</div>
       </IconButton>
     </Tooltip>
   );
--- packages/roomkit-react/src/Prebuilt/components/Footer/Interpreting/Int2Fr.tsx
+++ packages/roomkit-react/src/Prebuilt/components/Footer/Interpreting/Int2Fr.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { selectLocalPeerID, selectLocalPeerRole, useHMSActions, useHMSStore } from '@100mslive/react-sdk';
+import { selectLocalPeerID, useHMSActions, useHMSStore } from '@100mslive/react-sdk';
 import { Tooltip } from '../../../../Tooltip';
 import IconButton from '../../../IconButton';
 
@@ -9,12 +9,11 @@ const Int2FrButton = () => {
   const changeRole = () => {
     hmsActions.changeRoleOfPeer(localPeerId, 'terp-fr', true);
   };
-  const role = useHMSStore(selectLocalPeerRole);
+
   return (
     <Tooltip key="terp-fr" title="Interprétation vers le français">
       <IconButton onClick={changeRole} className="w-auto">
-        {role?.name === 'terp-en' ? <div className="text-xs text-slate-200 rounded-lg">FR</div> : null}
-        {role?.name === 'terp-fr' ? <div className="text-xs text-slate-200 rounded-lg bg-red-600">FR</div> : null}
+        <div className="text-xs text-slate-200 rounded-lg bg-red-600">FR</div>
       </IconButton>
     </Tooltip>
   );
